5ae350e4ac8884cae8d72f2a149617d6faec6db5
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-02-07 20:05:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,14 +3,14 @@ import time
 
 class Config(object):
     # Pyrogram Client
-    API_ID    = int(os.environ.get("API_ID", "24509589"))  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "717cf21d94c4934bcbe1eaa1ad86ae75") # ⚠️ Required
+    API_ID    = int(os.environ.get("API_ID", "29908998"))  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "3a0d920a6370b97671ea838d92dc559c") # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6151534135:AAHoxNXi9STwnkrO8EnJTqqEOks5qO3Z4fA") # ⚠️ Required
     
     # Other Configs
     BOT_START_TIME = time.time()
-    OWNER    = int(os.environ.get("OWNER", "6664582540"))  # ⚠️ Required
-    SUDO = list(map(int, os.environ.get("SUDO", "6280103226 6664582540").split()))  # ⚠️ Required
+    OWNER    = int(os.environ.get("OWNER", "6898413162"))  # ⚠️ Required
+    SUDO = list(map(int, os.environ.get("SUDO", "6898413162 6566179661").split()))  # ⚠️ Required
     # Web Response Config
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     PORT = int(os.environ.get("PORT", "8080"))
@@ -124,7 +124,7 @@ Tʜɪs ʙᴏᴛ ɪs sᴏʟᴇʟʏ ᴄʀᴇᴀᴛᴇ ᴏʀ ᴅᴇᴠᴇʟᴏᴘ�
 
     ABOUT_MSG = """
 - 𝖬𝗒 𝖭𝖺𝗆𝖾 : <a href=https://t.me/{}>{}</a>
-- 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <a href=@iam_daxx</a>
+- 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <a href=@II_BAD_MUNDA_II</a>
 - 𝖫𝗂𝖻𝗋𝖺𝗋𝗒 : Pyrogram
 - 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾 : 𝖯𝗒𝗍𝗁𝗈𝗇 𝟥
 - 𝖣𝖺𝗍𝖺𝖡𝖺𝗌𝖾 : 𝖬𝗈𝗇𝗀𝗈𝖣𝖡
